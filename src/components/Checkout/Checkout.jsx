3d141f16c268aa8567b9508c77688202d36ed503
gregmall <gregmall157@gmail.com>
2025-07-21 16:25:34 -0700

    new angle

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,34 +3,12 @@ import {UserContext} from '../../context/UserContextProvider'
 import { db } from '../../config/Config';
 import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import  { useNavigate} from 'react-router-dom';
-// import {Elements} from '@stripe/react-stripe-js';
-// import {loadStripe} from '@stripe/stripe-js'
-// import CheckoutForm from './CheckoutForm';
+
 const Checkout = () => {
-  // const [clientSecret, setClientSecret] = useState("")
-  // const stripePromise = loadStripe('pk_test_51PT6bf2MmEPWo5ZYKqKyzWL2etA3IZmlxGwTZQcnc0vrdDn0CNKUDYFwktBKGk8JXsOrvr01BHneFvFE6RGQzezx00jiuSmphH');
+ 
   const { user } = useContext(UserContext)
   const navigate = useNavigate()
-  // const URL = process.env.REACT_APP_URL
-  // useEffect(() => {
-  //   // Create PaymentIntent as soon as the page loads
-  //   console.log('here')
-  //   fetch(`${URL}/create-payment-intent`, {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => setClientSecret(data.clientSecret));
-  // }, [URL]);
-
-  // const appearance = {
-  //   theme: 'stripe',
-  // };
-  // const options = {
-  //   clientSecret,
-  //   appearance,
-  // };
+  
   const handleCheckout = () => {
     Confirm.show(
       'takes you to 3rd party CC page',
@@ -85,9 +63,7 @@ const Checkout = () => {
    </div>
   </div>
     
-    {/* <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements> */}
+
   </>
   )
 }
