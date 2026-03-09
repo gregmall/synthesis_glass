81cf373c826baf1e3bdad35eef513ca02a82e6dc
gregmall <gregmall157@gmail.com>
2026-03-09 15:57:39 -0700

    working on customers page

--- src/components/Admin/AdminPage.jsx
+++ src/components/Admin/AdminPage.jsx
@@ -13,6 +13,7 @@ function AdminPage() {
         <div className=' mt-4'>
           <Link to="/addproduct" className='text-2xl text-purple-700 hover:text-lime-500 mx-2'>Add item</Link>
           <Link to="/formsubmissions" className='text-2xl text-purple-700 hover:text-lime-500 mx-2'>Form Submissions</Link>
+          <Link to="/customers" className='text-2xl text-purple-700 hover:text-lime-500 mx-2'>Orders</Link>
         </div>
        </div>
     </div>
--- src/components/Admin/Customers.jsx
+++ src/components/Admin/Customers.jsx
@@ -1,18 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '../../config/Config';
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Customers() {
-  const [customers, setCustomers] = useState([]);
+  // const [customers, setCustomers] = useState([]);
 
-  useEffect(() => {
-    const fetchCustomers = async () => {
-      const snapshot = await db.collection('customers').get();
-      const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  // useEffect(() => {
+  //   const fetchCustomers = async () => {
+  //     const snapshot = await db.collection('customers').get();
+  //     const data = snapshot.docs.map(doc => ([doc.id]));
+  //     const payments = await db.collection('customers', data, 'payments').get();
+  //     const paymentsData = payments.docs.map(doc => doc.data());
       
-      setCustomers(data);
-    };
+  //     setCustomers(data);
+  //     console.log(paymentsData);
+  //   };
 
-    fetchCustomers();
+  //   fetchCustomers();
+  // }, []);
+
+  const [payments, setPayments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+  
+
+    async function fetchPayments() {
+      const customerId = "TrCn6sX8erNmc0HQ8sWuwF1mOfP2"
+      try {
+        setLoading(true);
+        const paymentsRef = collection(db, "customers", customerId, "payments");
+        const snapshot = await getDocs(paymentsRef);
+
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setPayments(data);
+        console.log(data);
+      } catch (err) {
+        console.error("Error fetching payments:", err);
+        setError(err);
+      } finally {
+        setLoading(false);
+       
+      }
+    }
+
+    fetchPayments();
   }, []);
 
   return (
@@ -20,7 +56,7 @@ export default function Customers() {
       <div className='p-4 w-full max-w-lg bg-white rounded-md'>
         <h1 className='border-b-2 mt-4 text-4xl text-center'>Customers</h1>
 
-        {customers.map(customer => {
+        {/* {customers.map(customer => {
           const mail = 'mailto:' + customer.email;
           return (
             <React.Fragment key={customer.id}>
@@ -29,7 +65,7 @@ export default function Customers() {
               <div className='border-b-4'>ID: {customer.id}</div>
             </React.Fragment>
           );
-        })}
+        })} */}
       </div>
     </div>
   )
